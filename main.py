62aef3edd354ff0d4da31bdec4af1b8aae6bea0d
Yuno <stepevade23@gmail.com>
2026-05-17 12:41:00 -0400

    added screen

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
 import sys
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
     sys.exit(1)
 
 
